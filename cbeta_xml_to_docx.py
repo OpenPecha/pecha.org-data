e61cf15c3078f053f09ef8cd9ac03c8d460251c6
drupchen <hhdrupchen@gmail.com>
2025-01-09 10:45:06 +0000

    update parser according to new folder conventions

--- cbeta_xml_to_docx.py
+++ cbeta_xml_to_docx.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import re
 
-from pecha_preparation_components import parse_cbeta_xml_triplets
+from pecha_preparation_components import parse_cbeta_xml_triplets, recursive_copy_metadata
 
 
 in_folder = Path('input/input_raw/kumarajiva/Gold Standard')
 out_folder = Path('output')
 parse_cbeta_xml_triplets(in_folder, out_folder)
+# recursive_copy_metadata(Path('input/metadata_template.xlsx'), Path('output/Gold Standard'))
--- pecha_preparation_components/raw_input_parsers/parse_cbeta_xml_triplets.py
+++ pecha_preparation_components/raw_input_parsers/parse_cbeta_xml_triplets.py
@@ -4,35 +4,48 @@ from collections import defaultdict
 from docx import Document  # from bayoo_docx
 
 
+def find_triplet(files):
+    tri = defaultdict(dict)
+    for f in files:
+        stem = f.stem
+        stem = stem if '.' not in stem else stem.split('.')[0]
+        if '.bo.zh' in f.name or '.bo.cn' in f.name or '.Tib.Chn' in f.name or '.BO.CN' in f.name:
+            tri[stem]['bo_zh'] = f
+        elif '.zh' in f.name or '.cn' in f.name or '.Chn' in f.name or '.CN' in f.name:
+            tri[stem]['zh'] = f
+        elif '.bo' in f.name or '.BO' in f.name or '.Tib' in f.name:
+            tri[stem]['bo'] = f
+        else:
+            print('there is a problem')
+            print(f)
+    return tri
+
+
 def parse_triplets(in_folder):
     # requires xml files triplets each in a sub_folder.  no more than 1 level
+    modern_folder_name = "Modern Chinese"
     total = {}
     for folder in Path(in_folder).glob("*"):
         files = list(folder.glob("*.xml"))
-        tri = defaultdict(dict)
-        for f in files:
-            stem = f.stem
-            stem = stem if '.' not in stem else stem.split('.')[0]
-            if '.bo.zh' in f.name or '.bo.cn' in f.name or '.Tib.Chn' in f.name or '.BO.CN' in f.name:
-                tri[stem]['bo_zh'] = f
-            elif '.zh' in f.name or '.cn' in f.name or '.Chn' in f.name or '.CN' in f.name:
-                tri[stem]['zh'] = f
-            elif '.bo' in f.name or '.BO' in f.name or '.Tib' in f.name:
-                tri[stem]['bo'] = f
-            else:
-                print('there is a problem')
-                print(f)
+        sub_dirs = [sub for sub in folder.glob("*") if sub.is_dir() and sub.name == modern_folder_name]
+        tri = {'trad': find_triplet(files), 'simp': None}
+        if sub_dirs:
+            files = list(sub_dirs[0].glob("*.xml"))
+            tri['simp'] = find_triplet(files)
         total[folder.name] = tri
 
     # check every triplet has 3 files and extract incomplete triplets
     incomplete = {}
-    for folder, tri in total.items():
-        keys = list(tri.keys())
-        for k in keys:
-            v = tri[k]
-            if len(v) != 3:
-                incomplete[folder] = {k: v}
-                del total[folder][k]
+    for folder, tr_sm in total.items():
+        for h, tri in tr_sm.items():
+            if not tri:
+                continue
+            keys = list(tri.keys())
+            for k in keys:
+                v = tri[k]
+                if len(v) != 3:
+                    incomplete[folder] = {k: v}
+                    del total[folder][h][k]
     return total, incomplete
 
 def parse_lang(in_file):
@@ -86,44 +99,65 @@ def align_triplet(triplet):
         aligned.append(entry)
     return name, aligned
 
-def write_documents(folder, filename, aligned):
+def write_documents(folder, filename, sim_trad, aligned):
+    # export to docx and parse footnotes
     if not folder.exists():
         folder.mkdir(parents=True, exist_ok=True)
     doc_bo = Document()
     doc_zh = Document()
+    line_num = 1
     for bo, zh in aligned:
         # add Tibetan
-        doc_bo.add_paragraph(bo, style='List Number')
+        doc_bo.add_paragraph(f'{line_num}. {bo}')
 
         # add Chinese text parsing footnotes as required
         if '^' in zh:
-            par = doc_zh.add_paragraph(style='List Number')
+            par = doc_zh.add_paragraph()
             # extract notes from the string
             footnotes = re.findall(r'\^[0-9]+\[([^\]]+)\]', zh)
             # replace notes by * and split on * to know where to add footnotes
-            parts = re.sub(r'\^[0-9]+\[[^\]]+?\]', '*', zh)
-            parts = re.split(r'(\*)', parts)
+            parts = re.sub(r'\^[0-9]+\[[^\]]+?\]', 'ᚸ', zh)
+            parts = re.split(r'(ᚸ)', parts)
             # adding text with footnotes at correct places
+            start_added = False
             f_num = 0
             for p in parts:
-                if p != '*':
-                    par.add_run(p)
+                if p != 'ᚸ':
+                    if not start_added:
+                        par.add_run(f'{line_num}. {p}')
+                        start_added = True
+                    else:
+                        par.add_run(p)
                 else:
                     try:
                         par.add_footnote(footnotes[f_num])
                         f_num += 1
                     except:
-                        print(zh)
+                        print("unable to parse note:", zh)
         else:
-            doc_zh.add_paragraph(zh, style='List Number')
-    doc_bo.save(folder / f'{filename}_bo.docx')
-    doc_zh.save(folder / f'{filename}_zh.docx')
+            doc_zh.add_paragraph(f'{line_num}. {zh}')
+        line_num += 1
+
+    if sim_trad == 'simp':
+        st = 'simplified_'
+    else:
+        st = ''
+    doc_bo.save(folder / f'{filename}_{st}bo.docx')
+    doc_zh.save(folder / f'{filename}_{st}zh.docx')
 
 def parse_cbeta_xml_triplets(in_folder, out_folder):
+    out_folder = out_folder / 'Gold Standard'
+
     triplets, incomplete = parse_triplets(in_folder)
     for work, parts in triplets.items():
         print(work)
-        out = out_folder / 'Gold Standard' / work
-        for _, tri in parts.items():
-            filename, aligned = align_triplet(tri)
-            write_documents(out, filename, aligned)
+        cur_out_folder = out_folder / work
+        for h, p in parts.items():
+            if not p:
+                continue
+            out = []
+            sorted_parts = [p[s] for s in sorted(p.keys())]
+            for tri in sorted_parts:
+                _, aligned = align_triplet(tri)
+                out.extend(aligned)
+            write_documents(cur_out_folder, work, h, out)
